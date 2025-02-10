6626f19352492d00a73e092bef236a6d84b5d254
Aliyah-Caulder <aliyahcaulder@gmail.com>
2025-02-10 11:21:13 -0500

    Completed Lab 6.

---
+++ LineNumbers.cpp
@@ -0,0 +1,40 @@
+/*
+
+-LineNumbers.cpp
+-
+-Aliyah Caulder
+-2/9/2025
+
+*/
+#include <iostream>
+#include <fstream>
+
+int main(){
+    //Variable declarations
+    std::string readFile;
+    std::string writeFile;
+    std::string text;
+    int lineNumber = 1;
+
+    //Prompt for user inputs
+    std::cout<<"Please enter the name of the file you want to read from: ";
+    std::getline(std::cin, readFile);
+    std::cout<<"Please enter the name of the file you want to rewrite the text to, with added line numbers: ";
+    std::getline(std::cin, writeFile);
+
+    //Read the selected file
+    std::ifstream inFile(readFile);
+
+    //Open the file to be written to
+    std::ofstream outFile(writeFile);
+
+    //Loop through the selected file, writing each line to the new file
+    while (std::getline(inFile, text)) {
+        outFile << lineNumber << ". " << text << std::endl;
+        lineNumber++;
+    }
+    inFile.close();
+    outFile.close();
+
+    return 0;
+}
