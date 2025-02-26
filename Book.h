6d4a0a6c9f3b4ad2be0cd55e32220001a02ec84a
Aliyah-Caulder <aliyahcaulder@gmail.com>
2025-02-25 22:24:35 -0500

    Completed the book header and tester

---
+++ Book.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+
+class Book
+{
+    private:
+        std::string codeNum;
+        std::string author;
+        std::string title;
+        int edition;
+        std::string pubCode;
+        double price;
+    
+    public:
+        //Constructor
+        inline Book(std::string cn, std::string au, std::string ti, int ed, std::string pc, double pr) {
+            codeNum = cn;
+            author = au;
+            title = ti;
+            edition = ed;
+            pubCode = pc;
+            price = pr;
+        }
+        //Getters
+        inline std::string getTitle(){
+            return title;
+        }
+        inline std::string getAuthor(){
+            return author;
+        }
+        inline double getPrice(){
+            return price;
+        }
+        inline int getEdition(){
+            return edition;
+        }
+        inline std::string getISBN(){
+            return codeNum;
+        }
+        inline std::string getPubCode(){
+            return pubCode;
+        }
+        //This method changes the price
+        inline void changePrice(double newPrice){
+            price = newPrice;
+        }
+};
